0831768977b76b036b230b446b3e8e5d7fa83d99
Carlos A. Michelén Ströfer <cmichel@sandia.gov>
2021-08-09 09:20:55 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="WecOptTool",
-    version="0.0.1",
+    version="0.0.2",
     author="Sandia National Labs",
     author_email="",
     description="",
