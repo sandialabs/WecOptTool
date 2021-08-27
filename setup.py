b40182002978d90b5e95e98b4a77b656f9a5c704
Michelen <cmichel@sandia.gov>
2021-08-27 09:24:16 -0600

    dependencies

--- setup.py
+++ setup.py
@@ -18,4 +18,19 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
+    install_requires=[
+        'pytest',
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'xarray',
+        'capytaine',
+        'autograd',
+        'mhkit',
+        'gmsh',
+        'pygmsh',
+    ],
+    dependency_links=[
+        'git+https://github.com/LHEEA/meshmagick.git@3.0'
+    ],
 )
