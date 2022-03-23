8ae5551e5db926763e34bc78f0cf8db67a18b443
Ryan Coe <rcoe@sandia.gov>
2022-03-23 12:59:43 -0600

    scale bounds

--- wecopttool/core.py
+++ wecopttool/core.py
@@ -699,6 +699,10 @@ class WEC:
         if x_opt_0 is None:
             x_opt_0 = np.random.randn(nstate_opt)
         x0 = np.concatenate([x_wec_0, x_opt_0])*scale
+        
+        if bounds is not None:
+            bounds.lb = bounds.lb*scale
+            bounds.ub = bounds.ub*scale
 
         # wave excitation force
         fd_we, td_we = wave_excitation(self.hydro, waves)
