4f19848ba44b9167d330d53b7b277ddb691dda54
dtgaebe <dtgaebe@sandia.gov>
2022-04-13 11:27:05 -0700

    added test for box wec

--- tests/test_waves.py
+++ tests/test_waves.py
@@ -10,8 +10,8 @@ from scipy.optimize import Bounds
 import wecopttool as wot
 from wecopttool.waves import pierson_moskowitz_spectrum as pm
 
-
-
+import gmsh
+import pygmsh
 
 
 @pytest.fixture()
@@ -48,9 +48,52 @@ def wec_wavebot():
 
     return wec_wavebot
 
-# @pytest.fixture()
-# def wec_box():
-#     return wec_box
+@pytest.fixture()
+def wec_box():
+    # water properties
+    rho = 1000.0
+
+    # frequencies
+    f0 = 0.05
+    nfreq = 18
+
+    # wave directions
+    wave_dirs = [0, 10, 20]
+
+    # create mesh
+    length = 1.2  
+    width = 0.7
+    height = 0.4  
+    mesh_size_factor = 0.5  # 1.0 for default, smaller to refine mesh
+
+    with pygmsh.occ.Geometry() as geom:
+        gmsh.option.setNumber('Mesh.MeshSizeFactor', mesh_size_factor)
+
+        box = geom.add_box(x0=[-length/2,
+                            -width/2,
+                            -height/2],
+                        extents=[length,
+                                    width,
+                                    height])
+        mesh = geom.generate_mesh()
+
+    fb = cpy.FloatingBody.from_meshio(mesh, name="Box_WEC")
+    fb.add_translation_dof(name="HEAVE")
+
+    # mass and hydrostativ stiffness
+    hs_data = wot.hydrostatics.hydrostatics(fb, rho=rho)
+    mass_33 = wot.hydrostatics.mass_matrix_constant_density(hs_data)[2, 2]
+    mass = np.atleast_2d(mass_33)
+    stiffness_33 = wot.hydrostatics.stiffness_matrix(hs_data)[2, 2]
+    stiffness = np.atleast_2d(stiffness_33)
+
+    # WEC
+    wec_box = wot.WEC(fb, mass, stiffness, f0, nfreq, rho=rho)
+
+    # BEM
+    wec_box.run_bem(wave_dirs)
+
+    return wec_box
 
 @pytest.fixture()
 def regular_wave(wec_wavebot):
@@ -71,7 +114,7 @@ def three_regular_waves(wec_wavebot):
     wave = wot.waves.wave_dataset(wec_wavebot.f0, wec_wavebot.nfreq,
                                   wave_dirs_deg)
     
-    wave.S.loc[dict(omega = freq*2*np.pi)] = amplitude**2
+    wave.S.loc[dict(omega = freq*2*np.pi)] = amplitude**2 / freq*2*np.pi
     wave.phase.loc[dict(omega = freq*2*np.pi)] = phase
     return wave
 
@@ -97,8 +140,6 @@ def irregular_wave(wec_wavebot):
         
     return irreg_wave
 
-def test_dummy():
-    assert 1 == 1
 
 def test_regular_waves_symmetric_wec(wec_wavebot, three_regular_waves):
     """Confirm that power from multiple regular waves that have the same
@@ -106,7 +147,8 @@ def test_regular_waves_symmetric_wec(wec_wavebot, three_regular_waves):
        the superposition of the waves 
        * the three individual waves all give the same results
        * those results match CC
-       * combined wave gives three times those results (position), 9?? times power
+       * combined wave gives three times those results (position), 
+       9?? times power
        """
     #PTO
     kinematics = np.eye(wec_wavebot.ndof)
@@ -157,10 +199,60 @@ def test_regular_waves_symmetric_wec(wec_wavebot, three_regular_waves):
     assert np.all(np.isclose(power_individual, sol_analytic_ind,rtol))
 
 
-# def test_regular_waves_asymmetric_wec(wec_box, three_regular_waves):
-#     """Confirm that power from different directions is unequal for asymmetric wec"""
-#     assert 1 == 1
+def test_regular_waves_asymmetric_wec(wec_box, three_regular_waves):
+    """Confirm that power from different directions is unequal for asymmetric wec"""
+    #PTO
+    kinematics = np.eye(wec_box.ndof)
+    pto = wot.pto.PseudoSpectralPTO(wec_box.nfreq, kinematics)
+    obj_fun = pto.average_power
+    wec_box.f_add = {'PTO': pto.force_on_wec}
 
+    #solve for combined wave
+    _, wec_fdom, _, _, power_combined, _ = wec_box.solve(
+                                        three_regular_waves,
+                                        obj_fun, 
+                                        nstate_opt = pto.nstate,
+                                        scale_x_wec = 1.0,
+                                        scale_x_opt = 0.01,
+                                        scale_obj = 1e-1,
+                                        optim_options={})
+    sol_analytic_com = -1*np.sum(np.abs(wec_fdom['excitation_force'][1:, :])**2 
+                            / (8*np.real(wec_box.hydro.Zi[:, 0, 0])))
+
+    #individual waves
+    sol_analytic_ind = np.zeros(three_regular_waves['wave_direction'].size)
+    power_individual = np.zeros(three_regular_waves['wave_direction'].size)
+    for nr_wave_dir in range(three_regular_waves['wave_direction'].size) :
+        _, wec_fdom, _, _, obj, _ = wec_box.solve(
+                    three_regular_waves.isel(wave_direction = [nr_wave_dir]), 
+                    obj_fun, 
+                    nstate_opt = pto.nstate,
+                    scale_x_wec = 1.0,
+                    scale_x_opt = 0.01,
+                    scale_obj = 1e-1,
+                    optim_options={})
+        power_individual[nr_wave_dir] = obj
+        sol_analytic_ind[nr_wave_dir] = \
+            -1*np.sum(np.abs(wec_fdom['excitation_force'][1:, :])**2  /
+            (8*np.real(wec_box.hydro.Zi[:, 0, 0]))
+                     )
+
+    #relative tolerance for assertion
+    rtol = 0.005
+    #check that analytic power for the combined waves matches
+    assert np.isclose(sol_analytic_com, power_combined, rtol)
+    #check that not all power results are the same
+    assert not np.all(np.isclose(power_individual, power_individual[0],rtol))
+    #check if combined wave yields expected power as function of 
+    # the individual waves
+    assert np.isclose(power_combined,
+                  np.sum(power_individual)*power_individual.size,
+                  rtol)
+
+def test_directional_regular_wave_decomposed(wec_wavebot):
+    """Confirm that power from an arbitrary direction matches the power
+        when decomposed into x- and y- direction"""
+    assert 1 == 1
 
 
 # def test_longcrested_wave_power(wec_wavebot):
