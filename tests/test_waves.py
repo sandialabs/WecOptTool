bug fix in spread function (degrees-radian) and unsort option

tests/test_waves.py
@@ -244,7 +244,7 @@ def test_regular_waves_asymmetric_wec(wec_box, three_regular_waves):
     #check that not all power results are the same
     assert not np.all(np.isclose(power_individual, power_individual[0],rtol))
     #check if combined wave yields expected power as function of 
-    # the individual waves
+    # the individual waves 
     assert np.isclose(power_combined,
                   np.sum(power_individual)*power_individual.size,
                   rtol)
@@ -273,9 +273,9 @@ def test_irregular_wave_power(wec_wavebot, irregular_wave):
 
 
 
-# def test_spectrum_energy(irregular_wave):
-#     """Confirm that energy is spread correctly accross wave directions
-#     * integral (sum) over all directions of the spread function gives (vector) 1
-#     * integral (sum) over all directions of the irregular wave (2D) spectrum gives the omni-direction spectrum (vector)
-#     """
-#     assert 1 == 1
+def test_spectrum_energy(irregular_wave):
+    """Confirm that energy is spread correctly accross wave directions
+    * integral (sum) over all directions of the spread function gives (vector) 1
+    * integral (sum) over all directions of the irregular wave (2D) spectrum gives the omni-direction spectrum (vector)
+    """
+    assert 1 == 1

wecopttool/core.py
@@ -1499,13 +1499,18 @@ def post_process_continuous_time(results: xr.DataArray
     return func
 
 
-def _degrees_to_radians(degrees: float | npt.ArrayLike
+def _degrees_to_radians(degrees: float | npt.ArrayLike,
+                        sort: bool = True,
                        ) -> float | np.ndarray:
     """Convert degrees to radians in range -π to π and sort.
     """
     radians = np.asarray(np.remainder(np.deg2rad(degrees), 2*np.pi))
     radians[radians > np.pi] -= 2*np.pi
-    radians = radians.item() if (radians.size == 1) else np.sort(radians)
+    # radians = radians.item() if (radians.size == 1) else np.sort(radians)
+    if (radians.size == 1):
+        radians = radians.item()
+    elif (sort):
+        radians = np.sort(radians)
     return radians
 
 def subsetclose(subset_a: float | npt.ArrayLike, 