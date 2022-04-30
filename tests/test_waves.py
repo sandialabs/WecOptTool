fix units in test

tests/test_waves.py
@@ -348,10 +348,9 @@ def test_spectrum_energy(irregular_wave, long_crested_wave):
     wave (2D) spectrum gives the omni-direction spectrum (vector).
     """
     wdir_step = (irregular_wave.wave_direction[1]
-                - irregular_wave.wave_direction[0]) *180/np.pi
+                - irregular_wave.wave_direction[0])
     # TODO: make units consistent in the wave xarray
     rtol= 0.01
-    assert np.allclose(wdir_step.values *
-                      irregular_wave.S.sum(dim = 'wave_direction').values,
-                      (long_crested_wave.S.values).T, rtol
-                      )
+    w1 = wdir_step.values * irregular_wave.S.sum(dim = 'wave_direction').values
+    w2 = (long_crested_wave.S.values).T
+    assert np.allclose(w1, w2, rtol)