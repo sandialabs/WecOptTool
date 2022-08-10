0c229437c47a9bc5d34f943c3379b6203f2fff2b
Ryan Coe <rcoe@sandia.gov>
2022-08-10 14:50:21 -0600

    docstring placeholders, most typehints

--- wecopttool/pto.py
+++ wecopttool/pto.py
@@ -11,7 +11,7 @@ dynamics.
 from __future__ import annotations
 
 
-from typing import Optional, TypeVar, Callable, Iterable
+from typing import Optional, TypeVar, Callable, Iterable, Union
 
 import autograd.numpy as np
 from autograd.builtins import isinstance, tuple, list, dict
@@ -19,8 +19,12 @@ from autograd.numpy import ndarray
 import numpy.typing as npt
 from scipy.linalg import block_diag
 from xarray import DataArray, Dataset
+import xarray as xr
+from tests.test_waves import ndbc_omnidirectional
+from datetime import datetime
 
-from wecopttool.core import WEC, complex_to_real, td_to_fd, dofmat_to_vec, vec_to_dofmat
+from wecopttool.core import WEC, real_to_complex, complex_to_real
+from wecopttool.core import td_to_fd, dofmat_to_vec, vec_to_dofmat
 from wecopttool.core import TWEC, TStateFunction, FArrayLike
 
 
@@ -29,11 +33,20 @@ TPTO = TypeVar("TPTO", bound="PTO")
 
 
 class PTO:
+    """A power take-off (PTO) object to be used in conjunction with a 
+    :python:`WEC` object.
+    TODO
+    """
 
-    def __init__(self, ndof, kinematics, controller=None, impedance=None, efficiency=None,
+    def __init__(self, 
+                 ndof: int, 
+                 kinematics: Union[Callable, npt.ArrayLike], 
+                 controller: Optional[Callable] = None, 
+                 impedance=None, #TODO - type?
+                 efficiency=None, #TODO - type?
                  names: Optional[list[str]] = None):
         """
-
+        TODO
         """
         self._ndof = ndof
         # names
@@ -74,56 +87,56 @@ class PTO:
 
     @property
     def ndof(self) -> int:
-        """Number of degrees of freedom.
-        """
+        """Number of degrees of freedom."""
         return self._ndof
 
     @property
     def names(self) -> ndarray:
-        """DOF Names.
-        """
+        """DOF Names."""
         return self._names
 
     @property
     def kinematics(self) -> TStateFunction:
-        """Kinemtaics function.
-        """
+        """Kinemtaics function."""
         return self._kinematics
 
     @property
     def force(self) -> TStateFunction:
-        """PTO force in PTO coordinates.
-        """
+        """PTO force in PTO coordinates."""
         return self._force
 
     @property
     def impedance(self) -> ndarray:
-        """Impedance matrix.
-        """
+        """Impedance matrix."""
         return self._impedance
 
     @property
     def efficiency(self) -> Callable[[FArrayLike, FArrayLike], FArrayLike]:
-        """Efficiency function.
-        """
+        """Efficiency function."""
         return self._efficiency
 
     @property
     def transfer_mat(self) -> ndarray:
-        """Transfer matrix.
-        """
+        """Transfer matrix."""
         return self._transfer_mat
 
-    def _tmat(self, wec, nsubsteps: int = 1):
+    def _tmat(self, wec: TWEC, nsubsteps: Optional[int] = 1):
         if nsubsteps==1:
             tmat = wec.time_mat
         else:
             tmat = wec.time_mat_nsubsteps(nsubsteps)
         return tmat
 
-    def _fkinematics(self, f_wec, wec, x_wec, x_opt=None, waves=None, nsubsteps: int = 1):
+    def _fkinematics(self, 
+                     f_wec, 
+                     wec: TWEC, 
+                     x_wec, 
+                     x_opt: Optional[ndarray] = None, 
+                     waves: Optional[Dataset] = None, 
+                     nsubsteps: Optional[int] = 1,):
         """ Return time-domain values in the PTO frame.
         `f_wec`: Fourier coefficients of some quantity "f" in the WEC frame.
+        TODO
         """
         time_mat = self._tmat(wec, nsubsteps)
         f_wec_td = np.dot(time_mat, f_wec)
@@ -133,33 +146,50 @@ class PTO:
         kinematics_mat = self.kinematics(wec, x_wec, x_opt, waves)
         return np.transpose(np.sum(kinematics_mat*f_wec_td, axis=1))
 
-    def position(self, wec: WEC, x_wec: npt.ArrayLike,
+    def position(self, 
+                 wec: TWEC, 
+                 x_wec: npt.ArrayLike,
                  x_opt: Optional[npt.ArrayLike],
-                 waves=None, nsubsteps: int = 1):
-        """Calculate the PTO position time-series."""
+                 waves: Optional[Dataset] = None,
+                 nsubsteps: Optional[int] = 1,):
+        """Calculate the PTO position time-series.
+        TODO
+        """
         pos_wec = wec.vec_to_dofmat(x_wec)
         return self._fkinematics(pos_wec, wec, x_wec, x_opt, waves, nsubsteps)
 
-    def velocity(self, wec: WEC, x_wec: npt.ArrayLike,
+    def velocity(self, 
+                 wec: TWEC, 
+                 x_wec: npt.ArrayLike,
                  x_opt: Optional[npt.ArrayLike],
-                 waves=None, nsubsteps: int = 1):
+                 waves: Optional[Dataset] = None,
+                 nsubsteps: Optional[int] = 1):
         """Calculate the PTO velocity time-series."""
         pos_wec = wec.vec_to_dofmat(x_wec)
         vel_wec = np.dot(wec.derivative_mat, pos_wec)
         return self._fkinematics(vel_wec, wec, x_wec, x_opt, waves, nsubsteps)
 
-    def acceleration(self, wec: WEC, x_wec: npt.ArrayLike,
+    def acceleration(self, 
+                     wec: TWEC, 
+                     x_wec: npt.ArrayLike,
                      x_opt: Optional[npt.ArrayLike],
-                     waves=None, nsubsteps: int = 1) -> np.ndarray:
-        """Calculate the PTO acceleration time-series."""
+                     waves: Optional[Dataset] = None,
+                     nsubsteps: Optional[int] = 1,
+                     ) -> np.ndarray:
+        """Calculate the PTO acceleration time-series.
+        TODO
+        """
         pos_wec = wec.vec_to_dofmat(x_wec)
         vel_wec = np.dot(wec.derivative_mat, pos_wec)
         acc_wec = np.dot(wec.derivative_mat, vel_wec)
         return self._fkinematics(acc_wec, wec, x_wec, x_opt, waves, nsubsteps)
 
-    def force_on_wec(self, wec: WEC, x_wec: npt.ArrayLike,
+    def force_on_wec(self, 
+                     wec: TWEC, 
+                     x_wec: npt.ArrayLike,
                      x_opt: Optional[npt.ArrayLike],
-                     waves=None, nsubsteps: int = 1):
+                     waves: Optional[Dataset] = None, 
+                     nsubsteps: Optional[int] = 1):
         force_td = self.force(wec, x_wec, x_opt, waves, nsubsteps)
         assert force_td.shape == (wec.nt, self.ndof)
         force_td = np.expand_dims(np.transpose(force_td), axis=0)
@@ -168,30 +198,55 @@ class PTO:
         kinematics_mat = np.transpose(kinematics_mat, (1,0,2))
         return np.transpose(np.sum(kinematics_mat*force_td, axis=1))
 
-    def mechanical_power(self, wec: WEC, x_wec: npt.ArrayLike,
+    def mechanical_power(self, 
+                         wec: TWEC, 
+                         x_wec: npt.ArrayLike,
                          x_opt: Optional[npt.ArrayLike],
-                         waves=None, nsubsteps: int = 1) -> np.ndarray:
+                         waves: Optional[Dataset] = None, 
+                         nsubsteps: Optional[int] = 1,
+                         ) -> np.ndarray:
         """Calculate the PTO power time-series in each PTO DOF
         for a given system state.
+        TODO
         """
         force_td = self.force(wec, x_wec, x_opt, waves, nsubsteps)
         vel_td = self.velocity(wec, x_wec, x_opt, waves, nsubsteps)
         return vel_td * force_td
 
-    def mechanical_energy(self, wec: WEC, x_wec: npt.ArrayLike,
+    def mechanical_energy(self, 
+                          wec: TWEC, 
+                          x_wec: npt.ArrayLike,
                           x_opt: Optional[npt.ArrayLike],
-              waves=None, nsubsteps: int = 1):
+                          waves: Optional[Dataset] = None, 
+                          nsubsteps: Optional[int] = 1,
+                          ) -> float:
+        """
+        TODO
+        """
         power_td = self.mechanical_power(wec, x_wec, x_opt, waves, nsubsteps)
         return np.sum(power_td) * wec.dt/nsubsteps
 
-    def mechanical_average_power(self, wec: WEC, x_wec: npt.ArrayLike,
+    def mechanical_average_power(self, 
+                                 wec: TWEC, 
+                                 x_wec: npt.ArrayLike,
                                  x_opt: Optional[npt.ArrayLike],
-                                 waves=None, nsubsteps: int = 1):
+                                 waves: Optional[Dataset] = None, 
+                                 nsubsteps: Optional[int] = 1,
+                                 ) -> float:
+        """TODO
+        """
         energy = self.mechanical_energy(wec, x_wec, x_opt, waves, nsubsteps)
         return energy / wec.tf
 
-    def power(self, wec: WEC, x_wec: npt.ArrayLike,
-              x_opt: Optional[npt.ArrayLike], waves=None, nsubsteps: int = 1):
+    def power(self, 
+              wec: TWEC, 
+              x_wec: npt.ArrayLike,
+              x_opt: Optional[npt.ArrayLike], 
+              waves: Optional[Dataset] = None, 
+              nsubsteps: Optional[int] = 1,
+              ) -> ndarray:
+        """TODO
+        """
         e1_td = self.force(wec, x_wec, x_opt, waves)
         q1_td = self.velocity(wec, x_wec, x_opt, waves)
         # convert e1 (PTO force), q1 (PTO velocity) to e2,q2
@@ -216,21 +271,31 @@ class PTO:
             power_out = power_out * self.efficiency(e2_td, q2_td)
         return power_out
 
-    def energy(self, wec: WEC, x_wec: npt.ArrayLike,
+    def energy(self, 
+               wec: TWEC, 
+               x_wec: npt.ArrayLike,
                x_opt: Optional[npt.ArrayLike],
-               waves=None, nsubsteps: int = 1):
+               waves: Optional[Dataset] = None, 
+               nsubsteps: Optional[int] = 1,
+               ) -> float:
+        """TODO"""
         power_td = self.power(wec, x_wec, x_opt, waves, nsubsteps)
         return np.sum(power_td) * wec.dt/nsubsteps
 
-    def average_power(self, wec: WEC, x_wec: npt.ArrayLike,
+    def average_power(self, 
+                      wec: TWEC, 
+                      x_wec: npt.ArrayLike,
                       x_opt: Optional[npt.ArrayLike],
-                      waves=None, nsubsteps: int = 1):
+                      waves: Optional[Dataset] = None, 
+                      nsubsteps: Optional[int] = 1,
+                      ) -> float:
         energy = self.energy(wec, x_wec, x_opt, waves, nsubsteps)
         return energy / wec.tf
 
 
 # power conversion chain
-def _make_abcd(impedance, ndof):
+def _make_abcd(impedance: npt.ArrayLike, ndof: int) -> ndarray:
+    """TODO"""
     z_11 = impedance[:ndof, :ndof, :]  # Fu
     z_12 = impedance[:ndof, ndof:, :]  # Fi
     z_21 = impedance[ndof:, :ndof, :]  # Vu
@@ -245,7 +310,9 @@ def _make_abcd(impedance, ndof):
     return np.block([[[abcd_11], [abcd_12]], [[abcd_21], [abcd_22]]])
 
 
-def _make_mimo_transfer_mat(impedance_abcd, ndof) -> np.ndarray:
+def _make_mimo_transfer_mat(impedance_abcd: npt.ArrayLike, 
+                            ndof: int,
+                            ) -> np.ndarray:
     """Create a block matrix of the MIMO transfer function.
     """
     elem = [[None]*2*ndof for _ in range(2*ndof)]
@@ -262,14 +329,29 @@ def _make_mimo_transfer_mat(impedance_abcd, ndof) -> np.ndarray:
 
 
 # controllers
-def controller_unstructured(pto, wec, x_wec, x_opt, waves=None, nsubsteps=1):
+def controller_unstructured(pto, 
+                            wec: TWEC, 
+                            x_wec: npt.ArrayLike, 
+                            x_opt: npt.ArrayLike, 
+                            waves: Optional[Dataset] = None, 
+                            nsubsteps=1) -> ndarray:
+    """TODO"""
     x_opt = np.reshape(x_opt, (-1, pto.ndof), order='F')
     tmat = pto._tmat(wec, nsubsteps)
     return np.dot(tmat, x_opt)
 
 
-def controller_pid(pto, wec, x_wec, x_opt, waves=None, nsubsteps=1,
-                   proportional=True, integral=True, derivative=True):
+def controller_pid(pto, 
+                   wec: TWEC, 
+                   x_wec: npt.ArrayLike, 
+                   x_opt: npt.ArrayLike,
+                   waves: Optional[Dataset] = None, 
+                   nsubsteps: Optional[int] = 1,
+                   proportional: Optional[bool] = True, 
+                   integral: Optional[bool] = True, 
+                   derivative: Optional[bool] = True,
+                   ) -> ndarray:
+    """TODO"""
     ndof = pto.ndof
     force_td = np.zeros([wec.nt, ndof])
     idx = 0
@@ -292,13 +374,27 @@ def controller_pid(pto, wec, x_wec, x_opt, waves=None, nsubsteps=1,
     return force_td
 
 
-def controller_pi(pto, wec, x_wec, x_opt, waves=None, nsubsteps=1):
+def controller_pi(pto, 
+                  wec: TWEC,
+                  x_wec: npt.ArrayLike, 
+                  x_opt: npt.ArrayLike, 
+                  waves: Optional[Dataset] = None, 
+                  nsubsteps: Optional[int] = 1,
+                  ) -> ndarray:
+    "TODO"
     force_td = controller_pid(pto, wec, x_wec, x_opt, waves, nsubsteps,
                                True, True, False)
     return force_td
 
 
-def controller_p(pto, wec, x_wec, x_opt, waves=None, nsubsteps=1):
+def controller_p(pto, 
+                 wec: TWEC, 
+                 x_wec: npt.ArrayLike, 
+                 x_opt: npt.ArrayLike, 
+                 waves: Optional[Dataset] = None, 
+                 nsubsteps: Optional[int] = 1,
+                 ) -> ndarray:
+    """TODO"""
     force_td = controller_pid(pto, wec, x_wec, x_opt, waves, nsubsteps,
                                True, False, False)
     return force_td
