3a386bd815bc9b14e78309397bed081f4db8c666
Ryan Coe <rcoe@sandia.gov>
2022-10-26 08:14:12 -0600

    use v3 interface for imageio

--- docs/source/make_theory_animations.py
+++ docs/source/make_theory_animations.py
@@ -8,7 +8,8 @@ Created on Wed Dec 29 14:55:52 2021
 import numpy as np
 import matplotlib.pyplot as plt
 
-import imageio.v2 as imageio  # TODO change code to use v3 and simply do `import imageio`.
+
+import imageio.v3 as iio
 import os
 import tempfile
 
@@ -22,8 +23,9 @@ if not os.path.exists(odir):
 
 gif_name = 'theory_animation'
 
-N = 30 # number of frames
-M = 10 # number of times to repeat final frame
+N = 30  # number of frames
+M = 10  # number of times to repeat final frame
+# will produce M+N total frames
 
 with tempfile.TemporaryDirectory() as tmpdirname:
 
@@ -37,9 +39,8 @@ with tempfile.TemporaryDirectory() as tmpdirname:
 
     xx = np.linspace(0, 2*np.pi, N)
     yy = 0.5*np.sin(xx)
-    ax.plot(xx,yy,'k--',
+    ax.plot(xx, yy, 'k--',
             label='correct solution')
-
 
     xdata, ydata = [], []
     ln, = ax.plot([], [], 'ro',
@@ -53,11 +54,11 @@ with tempfile.TemporaryDirectory() as tmpdirname:
     td_fnames = []
     fig.tight_layout()
 
-    for ind, (xd, yd) in enumerate(zip(xx,yy)):
+    for ind, (xd, yd) in enumerate(zip(xx, yy)):
         xdata.append(xd)
         ydata.append(yd)
         ln.set_data(xdata, ydata)
-        fname = os.path.join(tmpdirname,f"{gif_name}_td_{ind}.png")
+        fname = os.path.join(tmpdirname, f"{gif_name}_td_{ind}.png")
         # print(f"\t{fname}")
         fig.savefig(fname)
         td_fnames.append(fname)
@@ -77,10 +78,10 @@ with tempfile.TemporaryDirectory() as tmpdirname:
         plt.xticks(color='w')
         plt.yticks(color='w')
 
-        ax.plot(xx,yy,'k--',
+        ax.plot(xx, yy, 'k--',
                 label='correct solution')
 
-        ax.plot(xx,yy*(ind+1)/N, 'bs',
+        ax.plot(xx, yy*(ind+1)/N, 'bs',
                 markersize=5,
                 markerfacecolor='none',
                 label='pseudo-spectral solution',
@@ -88,23 +89,22 @@ with tempfile.TemporaryDirectory() as tmpdirname:
 
         ax.legend()
 
-        fname = os.path.join(tmpdirname,f"{gif_name}_ps_{ind}.png")
-        # print(f"\t{fname}")
+        fname = os.path.join(tmpdirname, f"{gif_name}_ps_{ind}.png")
         fig.tight_layout()
         fig.savefig(fname)
         ps_fnames.append(fname)
         plt.close(fig)
 
-
     fnames = {
-        'td':td_fnames,
-        'ps':ps_fnames,
-        }
+        'td': td_fnames,
+        'ps': ps_fnames,
+    }
 
     for key in fnames:
+        # repeat last frame M times
         fnames[key] = fnames[key] + [fnames[key][-1]]*M
-        oname = os.path.join(odir,f"{gif_name}_{key}.gif")
-        with imageio.get_writer(oname, mode='I') as writer:
-            for filename in fnames[key]:
-                image = imageio.imread(filename)
-                writer.append_data(image)
+        
+        frames = np.stack([iio.imread(filename) for filename in fnames[key]],
+                          axis=0)
+        gif_path = os.path.join(odir, f"{gif_name}_{key}.gif")
+        iio.imwrite(gif_path, frames)
