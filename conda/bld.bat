f8b8ba8196d19442764eb4fe898beb4558770972
mcdevin <mcdevin@sandia.gov>
2022-10-28 11:49:19 -0600

    local conda build successful with pygmsh, still need to test

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,3 +1,3 @@
-pip install pygmsh
+pip install https://files.pythonhosted.org/packages/6e/67/9d4ac4b0d683aaa4170da59a1980740b281fd38fc253e1830fde4dac3d4f/pygmsh-7.1.17-py3-none-any.whl
 python -m pip install .
 if errorlevel 1 exit 1
--- conda/build.sh
+++ conda/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pip install pygmsh
+pip install https://files.pythonhosted.org/packages/6e/67/9d4ac4b0d683aaa4170da59a1980740b281fd38fc253e1830fde4dac3d4f/pygmsh-7.1.17-py3-none-any.whl
 python -m pip install .     # Python command to install the script.
