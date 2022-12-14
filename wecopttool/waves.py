BUGFIX: Factor of sqrt(2) in irregular_wave, closes #127 (#167)
 * Improves testing for irregular waves
 * Fixes bug in irregular_wave (missing sqrt(2) factor)
 * Arg validation for fd_to_td

Co-authored-by: Ryan Coe <rcoe@sandia.gov>

wecopttool/waves.py
@@ -204,7 +204,7 @@ def long_crested_wave(
 
     values = efth.values
     values[values<0] = np.nan
-    amplitudes = np.sqrt(2*values * df)
+    amplitudes = np.sqrt(2 * values * df)
 
     attr = {
         'Wave type': 'Long-crested irregular',
@@ -240,7 +240,7 @@ def irregular_wave(efth: DataArray) -> DataArray:
 
     values = efth.values
     values[values<0] = np.nan
-    amplitudes = np.sqrt(values * df * dd)
+    amplitudes = np.sqrt(2 * values * df * dd)
 
     attr = {'Wave type': 'Irregular'}
 