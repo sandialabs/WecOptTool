2f5ab58a0105912ba24d7d32b4b106efb5ca9c70
dtgaebe <86246113+dtgaebe@users.noreply.github.com>
2023-06-26 16:01:06 -0700

    added impedance bode plot function

--- wecopttool/utilities.py
+++ wecopttool/utilities.py
@@ -8,7 +8,8 @@ from __future__ import annotations
 __all__ = [
     "intrinsic_impedance"
     "natural_frequency",
-    "plot_bem_results"
+    "plot_bem_results",
+    "plot_bode_intrinsic_impedance"
 ]
 
 
@@ -36,7 +37,8 @@ def natural_frequency(impedance: DataArray, freq: ArrayLike
     Parameters
     ----------
     impedance: DataArray
-        Complex intrinsic impedance matrix. 
+        Complex intrinsic impedance matrix produced by
+        :py:func:`wecopttool.hydrodynamic_impedance`.
         Dimensions: omega, radiating_dofs, influenced_dofs
     freq: list[float]
         Frequencies.
@@ -47,18 +49,11 @@ def natural_frequency(impedance: DataArray, freq: ArrayLike
         Natural frequencies.
     ind: int
         Index of natural frequencies.
-
-    Examples
-    --------
-    import capytaine as cpy
-    
-    hydrostatic_stiffness = lupa_fb.hydrostatic_stiffness
-    hydro_data = wecopttool.linear_hydrodynamics(bem_data, inertia_matrix, hydrostatic_stiffness)
-    hydro_data = wecopttool.check_linear_damping(hydro_data)
-    impedance = wecopttool.hydrodynamic_impedance(hydro_data)
     """
+
     #TODO: Only calculate for the dofs that have natural restoring force,
-    #heave, roll, pitch
+    #heave, roll, pitch, but issue is that dofs might not necessairly be called\
+    #  'Heave', 'Roll', 'Pitch', otherwise can print warning that results for other dofs are meaningless
 
     ind = np.diag(np.abs(impedance).argmin(dim = 'omega'))
     f_n = freq[ind]
@@ -140,3 +135,49 @@ def plot_hydrodynamic_coefficients(bem_data):
                 fig_am.delaxes(ax_am[i, j])
                 fig_rd.delaxes(ax_rd[i, j])
     fig_ex.legend(ex_handles, ex_labels, loc=(0.08, 0), ncol=2, frameon=False)
+
+
+def plot_bode_intrinsic_impedance(impedance: DataArray):
+    """Find the natural frequency based on the lowest magnitude impedance.
+
+    Parameters
+    ----------
+    impedance: DataArray
+        Complex intrinsic impedance matrix produced by
+        :py:func:`wecopttool.hydrodynamic_impedance`.
+        Dimensions: omega, radiating_dofs, influenced_dofs
+
+
+    """
+    radiating_dofs = impedance.radiating_dof.values
+    influenced_dofs = impedance.influenced_dof.values
+    mag = 20.0 * np.log10(np.abs(impedance))
+    phase = np.rad2deg(np.unwrap(np.angle(impedance)))
+    freq = impedance.omega.values   #TODO: freq units?!
+    fig, axes = plt.subplots(2*len(radiating_dofs), len(influenced_dofs),
+                                tight_layout=True, sharex=True, 
+                                figsize=(3*len(radiating_dofs), 3*len(influenced_dofs)), squeeze=False)
+    fig.suptitle('Impedance Bode Plots \n Mag (dB), Phase (deg)', fontweight='bold')
+
+    sp_idx = 0
+    for i, rdof in enumerate(radiating_dofs):
+        for j, idof in enumerate(influenced_dofs):
+            sp_idx += 1
+            axes[2*i, j].semilogx(freq, mag[i, j, :])    # Bode magnitude plot
+            axes[2*i+1, j].semilogx(freq, phase[i, j, :])    # Bode phase plot
+            axes[2*i, j].grid(True, which = 'both')
+            axes[2*i+1, j].grid(True, which = 'both')
+
+            if i == len(radiating_dofs)-1:
+                axes[2*i+1, j].set_xlabel(f'Frequency (Hz)', fontsize=10)
+            else:
+                axes[i, j].set_xlabel('')
+            if j == 0:
+                axes[2*i, j].set_ylabel(f'{rdof} \n Mag. (dB)', fontsize=10)
+                axes[2*i+1, j].set_ylabel(f'Phase. (deg)', fontsize=10)
+            else:
+                axes[i, j].set_ylabel('')
+            if i == 0:
+                axes[i, j].set_title(f'{idof}', fontsize=10)
+            else:
+                axes[i, j].set_title('')
