ca15bd9737b973fbdd2432d0e899242d07d16535
Michael Devin <michaelcdevin@outlook.com>
2024-05-14 21:09:19 -0400

    Dev version of documentation site (#347)
    * added initial file changes based on sphinx_multiversion docs and WEC-Sim implementation
    
    * removed sphinx-multiversion since it is no longer supported and made manual multiversion
    
    * now uses absolute paths, commented out linkcheck for debugging
    
    * fixed docstring errors in utilities module
    
    * updating files again that somehow got reverted
    
    * fixing path in conf.py
    
    * don't run tutorials (will revert later)
    
    * handle file moves correctly, fixed if statement to make other versions appear
    
    * fixed two bugs in versions template
    
    * reverted temp changes, changes latest to main
    
    * switched latest to main
    
    * main branch now in root directory of pages
    
    * fixed URLs with change from last commit
    
    * make other branches visible before building
    
    * switched main branch tag for more testing
    
    * fixed typo
    
    * switched dev branch to an existing branch
    
    * renamed main to latest, changed version.html file name to avoid confusion
    
    * added prints about moving files so Sphinx output isn't misleading
    
    * fixed typo with quotations
    
    * changed versions.html name back because that broke things I guess
    
    * modified contributing documentation to reflect changes
    
    * add logic to remove duplicate 'latest' branch
    
    * Fixed pathing when already on latest
    
    * remove typo
    
    * Troubleshooting complete, switching back to correct branches for deployment
    
    * Removed extra word in docstring
    
    * removed redundant function
    
    * fixed pathing so returns to same file (and fixes tutorial/API docs)
    
    * changed latest branch for demonstration
    
    * switched back latest branch for deployment

--- docs/clean_docs.py
+++ docs/clean_docs.py
@@ -6,11 +6,13 @@ build_dir = os.path.join(docs_dir, '_build')
 source_dir = os.path.join(docs_dir, 'source')
 example_dir = os.path.join(source_dir, '_examples')
 api_dir = os.path.join(source_dir, 'api_docs')
+pages_dir = os.path.join(docs_dir, 'pages')
 
 def clean():
     rmtree(example_dir, ignore_errors=True)
     rmtree(api_dir, ignore_errors=True)
     rmtree(build_dir, ignore_errors=True)
+    rmtree(pages_dir, ignore_errors=True)
 
 if __name__ == '__main__':
     clean()
