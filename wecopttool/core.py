cc804c8d26bdacf012dfc79d1ea03c5fccd88979
jtgrasb <87095491+jtgrasb@users.noreply.github.com>
2025-04-17 14:45:51 -0600

    Inertia matrix fix (#417)
    * use full body for inertia matrix, use only immersed part for hydrostatic stiffness
    
    * copy wec_im object and take immersed part
    
    * Update warning message
    
    * Add to warning for hydrostatics rotation center
    
    * Update hydrostatics process
    
    * if mass is none
    
    * Update warning messages

--- wecopttool/core.py
+++ wecopttool/core.py
@@ -2177,17 +2177,23 @@ def run_bem(
                       'wavelength': False,
                       'wavenumber': False,
                      }
-    wec_im = fb.copy(name=f"{fb.name}_immersed").keep_immersed_part()
+    wec_im = fb.copy()
     wec_im = set_fb_centers(wec_im, rho=rho)
     if not hasattr(wec_im, 'inertia_matrix'):
-        _log.warning('FloatingBody has no inertia_matrix field. ' + 
-                     'If the FloatingBody mass is defined, it will be ' + 
-                     'used for calculating the inertia matrix here. ' + 
-                     'Otherwise, the neutral buoyancy assumption will ' + 
-                     'be used to auto-populate (inertial properties ' +
-                     'based only on the immersed part). We recommend ' +
-                     'inputting the correct inertial properties.')
+        if wec_im.mass is None:
+            wec_im.mass = rho*wec_im.copy().keep_immersed_part().volume
+            _log.warning('FloatingBody has no inertia_matrix or mass ' +
+                     'field. The mass will be calculated based on a ' +
+                     'neutral buoyancy assumption. The inertia matrix ' +
+                     'will be calculated assuming a solid and constant ' +
+                     'density body.')
+        else:
+            _log.warning('FloatingBody has no inertia_matrix field. ' + 
+                     'The FloatingBody mass is defined and will be ' +
+                     'used for calculating the inertia matrix.')
         wec_im.inertia_matrix = wec_im.compute_rigid_body_inertia(rho=rho)
+    wec_im = wec_im.keep_immersed_part()
+    wec_im.name = f"{wec_im.name}_immersed"
     if not hasattr(wec_im, 'hydrostatic_stiffness'):
         _log.warning('FloatingBody has no hydrostatic_stiffness field. ' +
                      'Capytaine will auto-populate the hydrostatic ' +
@@ -2588,7 +2594,7 @@ def set_fb_centers(
                 log_str = (
                     "Using the center of gravity (COG) as the rotation center " +
                     "for hydrostatics. Note that the hydrostatics do not use the " +
-                    "axes defined by the Floating Body degrees of freedom, and the " + 
+                    "axes defined by the FloatingBody degrees of freedom, and the " + 
                     "rotation center should be set manually when using Capytaine to " + 
                     "calculate hydrostatics about an axis other than the COG.")
             setattr(fb, property, def_val)
